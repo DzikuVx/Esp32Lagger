919b8f87aac406013ad0a885ad3ddc8a13d37d6d
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2021-10-03 21:07:14 +0200

    Logging improvements

--- Esp32Lagger.ino
+++ Esp32Lagger.ino
@@ -22,6 +22,8 @@ const char* password = "123456789";
  * Default is 256 bytes, but increasing the value makes much more sense
  */
 #define RX_BUFFER_SIZE 512
+#define BUFFER_SIZE 512
+#define MIN_WRITE_SIZE 16    // Do not write less than this
 
 AsyncWebServer server(80);
 HardwareSerial hSerial(1);
@@ -33,8 +35,8 @@ File file;
 
 uint32_t lastByteReceived;
 uint32_t nextCleanupMs;
+uint32_t nextStatsMs;
 
-#define BUFFER_SIZE 255
 
 String nextFileName;
 
@@ -167,6 +169,8 @@ void setup()
     hSerial.flush();
 }
 
+uint32_t sdCardTime;
+
 void loop()
 { 
 
@@ -181,36 +185,61 @@ void loop()
 
     byte buffer[BUFFER_SIZE];
 
-    byte dataLength = hSerial.read(buffer, sizeof(buffer));
+    uint32_t availabeBytes = hSerial.available();
 
-    if (dataLength > 0 && millis() > 500) {
+    /*
+     * Read data from serial port if there are at last MIN_WRITE_SIZE bytes
+     * or every 500ms to get final portion of the log
+     */
+    if (availabeBytes >= MIN_WRITE_SIZE || lastByteReceived + 500 < millis()) {
+        byte dataLength = hSerial.read(buffer, sizeof(buffer));
 
-        lastByteReceived = millis();
+        if (dataLength > 0 && millis() > 500) {
 
-        if (!isFileOpened) {
-            file = SD.open(nextFileName, FILE_WRITE);
-            isFileOpened = true;
+            lastByteReceived = millis();
 
-            nextCleanupMs = millis() + 2000; //Cleanup ever 2s
-            Serial.println("File created " + String(millis()) + " " + String(dataLength));
+            if (!isFileOpened) {
+                file = SD.open(nextFileName, FILE_WRITE);
+                isFileOpened = true;
+
+                nextCleanupMs = millis() + 2000; //Cleanup ever 2s
+                Serial.println("File created " + String(millis()) + " " + String(dataLength));
+            }
+
+            if (!file) {
+                Serial.println("error opening file");
+            } else {
+
+                uint32_t start = micros();
+
+                file.write(buffer, dataLength);
+
+                if (dataLength > 1) {
+                    sdCardTime += (micros() - start); //Log how many microseconds it took to write to SD card
+
+                    // Serial.println(String(dataLength) + " " + String(micros() - start));
+                }
+            }
+
+            /*
+            * From time to time close and reopen the file to synch all changes to the file system
+            */
+            if (file && millis() > nextCleanupMs) {
+
+                file.flush();
+                Serial.println("Cleanup");
+                nextCleanupMs = millis() + 2000;
+            }
         }
+    }
 
-        if (!file) {
-            Serial.println("error opening file");
-        } else {
-            file.write(buffer, dataLength);
-        }
-
-        /*
-         * From time to time close and reopen the file to synch all changes to the file system
-         */
-        if (file && millis() > nextCleanupMs) {
-
-            file.flush();
-            Serial.println("Cleanup");
-            nextCleanupMs = millis() + 2000;
-        }
-
+    /*
+     * Write SD card usage statistics
+     */
+    if (millis() > nextStatsMs) {
+        nextStatsMs = millis() + 1000;
+        Serial.println("SD card time: " + String((sdCardTime / 1000000.0f) * 100) + "%");
+        sdCardTime = 0;
     }
 
 }
